c51631e8dae20a0cc3ad2e19b4eab8a46d4cd2dd
Mitro9070 <my@mitro9070.ru>
2024-11-21 13:38:20 +0300

    Исправлен футер

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,34 @@
-// Импорт CSS стилей для футера
-import '../styles/Footer.css';
-
 function Footer() {
     return (
-        <div className="footer">
+        <div style={{
+            width: '1200px',
+            display: 'grid',
+            gridTemplateColumns: '33% 33% 33%',
+            gridGap: '10px',
+            fontSize: '12px',
+            fontWeight: '500',
+            padding: '20px',
+            backgroundColor: '#fff',
+            borderTop: '1px solid #e5e5e5',
+            position: 'static',
+            bottom: '0',
+            left: '5px',
+            marginTop: '10px'
+        }}>
             {/* Первая колонка футера */}
-            <div className="column-footer">
+            <div style={{ textAlign: 'left', width: '100%' }}>
                 <p>+7 (495) 120-11-25</p>
                 <p>123112, Москва, Пресненская наб. 8, с.1</p>
                 <p>Москва-Сити, МФК “Город Столиц”</p>
             </div>
            {/* Вторая колонка футера */}
-           <div className="center-column-footer">
+           <div style={{ textAlign: 'left', width: '100%' }}>
                 <p>Секретариат secretary@katusha-it.ru</p>
                 <p>IT it@saturn-sw.ru</p>
                 <p>Реклама PR@Katusha-it.ru</p>
             </div>
             {/* Третья колонка футера */}
-            <div className="last-column-footer">
+            <div style={{ textAlign: 'left', width: '100%' }}>
                 <p>Основной Telegram @katushait</p>
                 <p>Поддержка @BestServiceKatushaIT</p>
                 <p>YouTube Название</p>
